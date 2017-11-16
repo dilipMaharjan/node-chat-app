c2c8e34e4f58424e6da4e92ddf181018bdd9980f
crazdrms <dilipmaharjann@gmail.com>
2017-11-15 21:46:33 -0500

    server and client connection and disconnection

--- server/server.js
+++ server/server.js
@@ -1,15 +1,30 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, './../public');
 
 const app = express();
 
-const port =process.env.PORT||3000;
-const publicPath = path.join(__dirname, './../public');
+var server = http.createServer(app);
+var io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('New user connected.');
+    socket.on('disconnect', () => {
+        console.log('Client disconnected.');
+    });
+});
+
+
+
 
 //middleware registration to server html
 app.use(express.static(publicPath));
 
 //listening at port 3000
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`App started at ${port}`)
 });
