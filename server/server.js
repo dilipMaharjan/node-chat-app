2ca97f90d68c2c0c7b9135e42b9450d054dc2f5f
crazdrms <dilipmaharjann@gmail.com>
2017-11-16 21:23:27 -0500

    emitting message to everyone excluding onself

--- server/server.js
+++ server/server.js
@@ -14,15 +14,23 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', { from: 'Admin', text: 'Welcome New User.', createdAt: new Date().getTime() });
+  socket.broadcast.emit('newMessage', { from: 'Admin', text: 'New User joined.', createdAt: new Date().getTime() });
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-    io.emit('newMessage', {
+    //   io.emit('newMessage', {
+    //     from: message.from,
+    //     text: message.text,
+    //     createdAt: new Date().getTime()
+    //   });
+    // });
+    socket.broadcast.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
   });
-
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
